6d40deb29082878750a9b67babaf7f64461463f1
harshpandey002 <coding.harshp@gmail.com>
2023-04-02 02:47:30 +0530

    Handle Events

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import { Client, GatewayIntentBits } from 'discord.js';
 
 config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
 
 client.login(process.env.BOT_TOKEN);
+
+client.once('ready', () => {
+  console.log(`${client.user.tag} is logged in!`);
+});
+
+client.on('messageCreate', (message) => {
+  console.log(message.content);
+});
+
+client.on('channelCreate', (channel) => {
+  console.log(channel);
+  console.log(channel.name);
+});
