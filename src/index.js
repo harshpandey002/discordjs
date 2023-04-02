82b8f04e7575c0def7b083780e02dccb5bedc07b
harshpandey002 <coding.harshp@gmail.com>
2023-04-02 22:37:59 +0530

    login bot

--- src/index.js
+++ src/index.js
@@ -1,32 +1,16 @@
 import { config } from 'dotenv';
-import { Client, GatewayIntentBits, REST, Routes } from 'discord.js';
+import { Client, GatewayIntentBits } from 'discord.js';
 
 config();
 
 const TOKEN = process.env.BOT_TOKEN;
-const CLIENT_ID = process.env.CLIENT_ID;
-const GUILD_ID = process.env.GUILD_ID;
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
 });
 
-const commands = [
-  {
-    name: 'poke',
-    description: 'Helps you poke a little',
-  },
-];
-
-const rest = new REST({ version: '10' }).setToken(TOKEN);
-
 async function main() {
   try {
-    console.log('Started refreshing application (/) commands.');
-
-    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
-      body: commands,
-    });
     client.login(TOKEN);
   } catch (error) {
     console.log(error);
